729f78e581b91366178a87835b3315ec51e04181
Brian Medley <pub-github@bmedley.org>
2022-05-01 08:38:21 -0400

    first commit

---
+++ Server/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
